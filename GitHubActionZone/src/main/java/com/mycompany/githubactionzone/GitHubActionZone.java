0aa0998f0455977d761db4e1268c0c805a065990
Iviwe-alt <ST10533801@myemeris.edu.za>
2026-03-19 10:27:36 +0200

    automeated testing example

---
+++ GitHubActionZone/src/main/java/com/mycompany/githubactionzone/GitHubActionZone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubactionzone;
+
+/**
+ *
+ * @author majin
+ */
+public class GitHubActionZone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
